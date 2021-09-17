cf54482413b43c6eda16df0666322d7e0cd831e9
Anthony Piva <anthony@asseinfo.com.br>
2021-09-17 17:55:23 -0300

    Removed authentication for tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: 'anthony', password: 'secret', except: [:index, :show]
-
   def index
     @articles = Article.all
   end
